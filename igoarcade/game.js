a73af3e4d95f6d2ba6946a686e84f1896436b92a
Zenigata <lhaagounet@gmail.com>
2015-12-06 02:23:02 +0100

    Displaying the finished goban.

--- igoarcade/game.js
+++ igoarcade/game.js
@@ -5,7 +5,7 @@ require('wgo');
 var pachi = require('pachi');
 
 // constantes
-var boardSize = 9;
+var boardSize = 7;
 var board, game, gtp;
 var alreadyPassed = false;
 var markers;
@@ -84,8 +84,9 @@ var computerPlay = function() {
     console.log('move: '+response);
     if (response && response !== '') {
       if (response === 'resign') {
+        disableGoban();
+        gtp.exit();
         displayMessage('L\'ordinateur abandonne');
-        gameEnd();
       }
       else if (response === 'pass') {
         if (alreadyPassed) {
@@ -146,8 +147,8 @@ var playerPass = function() {
 };
 
 var gameEnd = function() {
-  getScore();
   disableGoban();
+  displayTerritory();
 };
 
 var getScore = function() {
@@ -165,6 +166,18 @@ var getScore = function() {
       message += ' !';
       displayMessage(message);
     }
+    gtp.exit();
+  });
+};
+
+var displayTerritory = function() {
+  gtp.send('final_status_list dead', function(error, response) {
+    var originalPosition = game.getPosition();
+    var position = game.getPosition().clone();
+
+    //calculate(position);
+    updateScoreBoard(position, originalPosition, response);
+    getScore();
   });
 };
 
@@ -206,6 +219,112 @@ var letterToNumberCoordinates = function(l, y) {
   var x = l.charCodeAt(0) - "A".charCodeAt(0);
   if(l.charCodeAt(0) >= "I".charCodeAt(0)) x--;
   return {x: x, y: Math.abs(y-board.size)};
+};
+
+/* WGo */
+var state = {
+	UNKNOWN: 0,
+	BLACK_STONE: 1, // must be equal to WGo.B
+	WHITE_STONE: -1, // must be equal to WGo.W
+	BLACK_CANDIDATE: 2,
+	WHITE_CANDIDATE: -2,
+	BLACK_NEUTRAL: 3,
+	WHITE_NEUTRAL: -3,
+	NEUTRAL: 4
+};
+
+var calculate = function(position) {
+	var p, s, t, b, w, change;
+
+	// 1. create testing position, empty fields has flag ScoreMode.UNKNOWN
+	p = position;
+
+	// 2. repeat until there is some change of state:
+	change = true;
+	while(change) {
+		change = false;
+
+		// go through the whole position
+		for(var i = 0; i < p.size; i++) {
+			//var str = "";
+			for(var j = 0; j < p.size; j++) {
+				s = p.get(j,i);
+
+				if(s == state.UNKNOWN || s == state.BLACK_CANDIDATE || s == state.WHITE_CANDIDATE) {
+					// get new state
+					t = [p.get(j-1, i), p.get(j, i-1), p.get(j+1, i), p.get(j, i+1)];
+					b = false;
+					w = false;
+
+					for(var k = 0; k < 4; k++) {
+						if(t[k] == state.BLACK_STONE || t[k] == state.BLACK_CANDIDATE) b = true;
+						else if(t[k] == state.WHITE_STONE || t[k] == state.WHITE_CANDIDATE) w = true;
+						else if(t[k] == state.NEUTRAL) {
+							b = true;
+							w = true;
+						}
+					}
+
+					t = false;
+
+					if(b && w) t = state.NEUTRAL;
+					else if(b) t = state.BLACK_CANDIDATE;
+					else if(w) t = state.WHITE_CANDIDATE;
+
+					if(t && s != t) {
+						change = true;
+						p.set(j, i, t);
+					}
+				}
+				//str += (p.get(j,i)+5)+" ";
+			}
+			//console.log(str);
+		}
+		//console.log("------------------------------------------------------------");
+	}
+};
+
+var updateScoreBoard = function(position, originalPosition, deadStones) {
+	var score = {
+		black: [],
+		white: [],
+		neutral: []
+	}
+
+  var s;
+
+  // dead stones
+  if (deadStones) {
+    var caps = deadStones.split(' ');
+    for (var i=0; i<caps.length; i++) {
+      var move = letterToNumberCoordinates(caps[i].charAt(0), caps[i].charAt(1));
+      s = originalPosition.get(move.x, move.y);
+      board.removeObjectsAt(move.x, move.y);
+      if (s != state.WHITE_STONE) {
+        score.white.push({x: move.x, y: move.y, type: "outline", c: WGo.B});
+        position.set(move.x, move.y, state.WHITE_CANDIDATE);
+      } else {
+        score.black.push({x: move.x, y: move.y, type: "outline", c: WGo.W});
+        position.set(move.x, move.y, state.BLACK_CANDIDATE);
+      }
+    }
+  }
+
+  calculate(position);
+
+	for(var i = 0; i < position.size; i++) {
+		for(var j = 0; j < position.size; j++) {
+			s = position.get(i,j);
+
+			if(s == state.BLACK_CANDIDATE) score.black.push({x: i, y: j, type: "mini", c: WGo.B});
+			else if(s == state.WHITE_CANDIDATE) score.white.push({x: i, y: j, type: "mini", c: WGo.W});
+			else if(s == state.NEUTRAL) score.neutral.push({x: i, y: j});
+		}
+	}
+
+	board.addObject(score.black);
+	board.addObject(score.white);
+
 };
 
 newGame();
